a72ed617b397da31928cf1580a54ba28d97e7f1e
Kong Dinh <alexvinh2911@gmail.com>
2025-03-19 11:05:26 +0700

    Add CORS middleware to allow requests from frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"; // Import cors
 import userRoute from "./routes/userRoute.js";
 import { pool } from "./config/pool.js";
 
@@ -8,6 +9,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+// Configure CORS to allow requests from your frontend
+const corsOptions = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200,
+  onSuccess: () => {
+    if (corsOptions.optionsSuccessStatus === 200) {
+      console.log("CORS configuration successful with status 200");
+    }
+  },
+};
+
+app.use(cors(corsOptions)); // Use cors middleware with options
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,7 +28,4 @@ app.use("/users", userRoute);
 
 app.listen(PORT, async () => {
   console.log("Server is running on port " + PORT);
-  const result = await pool.query("SELECT * FROM users");
-
-  console.log(result.rows);
 });
