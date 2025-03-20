28ab427cd59c241c4e63be8caec291ec46168998
= <phuongnguyen20904@gmail.com>
2025-03-20 16:10:35 +0700

    feat: add mobile Header and HeaderBot components for navigation

---
+++ frontend/src/components/Mobile/HeaderBot.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { MdHomeFilled } from "react-icons/md";
+
+function HeaderBotMB() {
+    return (
+        <div className="w-full h-14 fixed bottom-0 left-0 bg-color-light z-50 md:hidden
+                        flex items-center justify-between px-4 border-t-2 border-gray-200
+                        ">
+            <MdHomeFilled 
+                title="Home"
+                size={33} 
+                className='text-black cursor-pointer'/>
+        </div>
+    )
+}
+
+export default HeaderBotMB
