ae74f445f0465f5adf05f88d3611c73deefa4d98
= <phuongnguyen20904@gmail.com>
2025-04-14 14:48:45 +0700

    feat (CORS): simplify CORS configuration by removing unnecessary success callback

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,13 +11,10 @@ const PORT = process.env.PORT;
 
 // Configure CORS to allow requests from your frontend
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5173",              
   optionsSuccessStatus: 200,
-  onSuccess: () => {
-    if (corsOptions.optionsSuccessStatus === 200) {
-      console.log("CORS configuration successful with status 200");
-    }
-  },
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"], 
 };
 
 app.use(cors(corsOptions)); // Use cors middleware with options
