6826c2c84db1c7e0198f0d40e6368002a661f384
= <phuongnguyen20904@gmail.com>
2025-04-14 10:44:00 +0700

    feat (Context): add ThemeContext and ThemeProvider for managing dark/light theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,22 @@ import LogInPage from "./pages/LogInPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 import ProfilePage from "./pages/ProfilePage";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LogInPage />} />
-        <Route element={<Layout />}>
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile" element={<ProfilePage/>} />
-        </Route>
-      </Routes>
-  </Router>
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<LogInPage />} />
+          <Route element={<Layout />}>
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
