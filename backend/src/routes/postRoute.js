4e0f545f679c53a3be03d37a7d09ae0e7ef4f056
= <phuongnguyen20904@gmail.com>
2025-04-17 21:14:37 +0700

    feat(routes): add post routes and update user routes for authentication

---
+++ backend/src/routes/postRoute.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+  createPost,
+  getAllPosts,
+  likePost,
+  commentPost,
+  getComments,
+  unlikePost,
+  deletePost,
+  getSinglePost,
+  editPost,
+} from "../controller/postController.js";
+
+const router = Router();
+
+router.post("/", createPost);
+router.get("/", getAllPosts);
+router.post("/like", likePost);
+router.post("/comment", commentPost);
+router.get("/:postId/comments", getComments);
+router.post("/unlike", unlikePost);
+router.delete("/:id", deletePost);
+router.get("/:id", getSinglePost);
+router.put("/:id", editPost);
+
+export default router;
--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -4,20 +4,20 @@ import {
   loginUser,
   getUserProfile,
   updateUserProfile,
-  getAllUsersProfile,
   getCurrentUserProfile,
+  logoutUser,
 } from "../controller/userController.js";
-import { authenticateToken } from "../middleware/authMiddleware.js"; 
-
+import { authenticateUser } from "../middleware/authenticateUser.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
 // Protect the following routes with authentication middleware
-router.get("/profile/:id", authenticateToken, getUserProfile);
-router.get("/profile", authenticateToken, getCurrentUserProfile);
-router.put("/profile/:id", authenticateToken, updateUserProfile);
+router.get("/profile/:id", authenticateUser, getUserProfile);
+router.get("/profile",authenticateUser , getCurrentUserProfile);
+router.put("/profile/me/update",authenticateUser , updateUserProfile);
 
 export default router;
