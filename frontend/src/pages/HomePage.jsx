93b703aae4d7f356cdb1decd57cb16c219996727
= <phuongnguyen20904@gmail.com>
2025-04-18 12:56:33 +0700

    refactor(homepage): restructure layout for improved responsiveness and organization

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,11 +3,16 @@ import PostCard from "../components/PostCard";
 
 function HomePage() {
     return (
-    <div className="md:ml-16">
-        <div className=" h-28 border"></div>
-        <PostCard/>
-        <PostCard/> 
-        <PostCard/>
+    <div className="flex w-full border">    
+        <div className="md:ml-16 lg:ml-0 lg:flex-3 flex-1">
+            <div className=" h-28 border"></div>
+            <PostCard/>
+            <PostCard/> 
+            <PostCard/>
+        </div>
+        
+        {/* Sidebar phải */}
+        <div className="lg:flex-1 hidden lg:block border"></div>
     </div>
     );
 }
