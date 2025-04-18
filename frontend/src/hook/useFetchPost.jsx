d9eeb75832a330f035be3b45ce2bdfd82bc93681
= <phuongnguyen20904@gmail.com>
2025-04-18 12:55:39 +0700

    feat(posts): add useInfinitePosts hook for infinite scrolling and fetchPost service

---
+++ frontend/src/hook/useFetchPost.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState, useRef, useCallback } from "react";
+import fetchPost from "../services/fetchPost";
+
+const useInfinitePosts = () => {
+    const [posts, setPosts] = useState([]);
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+    const observerRef = useRef();
+
+    const lastPostRef = useCallback(
+        (node) => {
+        if (loading) return;
+
+        if (observerRef.current) observerRef.current.disconnect();
+
+        observerRef.current = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting && hasMore) {
+            setPage((prev) => prev + 1);
+            }
+        });
+
+        if (node) observerRef.current.observe(node);
+        },
+        [loading, hasMore]
+    );
+
+    useEffect(() => {
+        const loadPosts = async () => {
+        if (loading || !hasMore) return;
+        setLoading(true);
+        try {
+            const data = await fetchPost(page);
+            if (data.posts.length === 0) {
+            setHasMore(false);
+            } else {
+                setPosts(prev => {
+                    const newPosts = data.posts.filter(p => !prev.some(existing => existing.id === p.id));
+                    return [...prev, ...newPosts];
+                });
+            }
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+        };
+
+        loadPosts();
+    }, [page]);
+
+    return { posts, loading, lastPostRef };
+};
+
+export default useInfinitePosts;
