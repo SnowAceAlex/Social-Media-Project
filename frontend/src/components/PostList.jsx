30888cde4acd1650aa31a3cfea1b6c3ec8f22ffc
= <phuongnguyen20904@gmail.com>
2025-04-20 00:07:34 +0700

    fix(postlist): improve empty state message and loading logic in PostList component

--- frontend/src/components/PostList.jsx
+++ frontend/src/components/PostList.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import useInfinitePosts from "../hook/useFetchPost";
 import Post from "./Post";
+import { getCurrentUser } from "../helpers/getCurrentUser";
+import { CiCamera } from "react-icons/ci";
 
 function PostList({ profile = null, loadingProfile = false, userId = null }) {
+    const {currentUser} = getCurrentUser();
     const { posts, loading, lastPostRef } = useInfinitePosts(userId);
 
     return (
         <div className="flex flex-col w-full">
-            {posts.map((post, index) => {
-            const isLast = index === posts.length - 1;
-            return (
-                <div
-                key={post.id}
-                ref={isLast ? lastPostRef : null}
-                className="py-2 border-b-[1px] border-light-border dark:border-dark-border"
-                >
-                <Post post={post} profile={profile} loading={loadingProfile} />
+            {!loading && posts.length === 0 && (
+                <div className="mt-4 text-gray-500 dark:text-gray-400 py-8 flex items-center justify-center flex-col gap-6">
+                    <div className="p-4 rounded-full text-gray-500 dark:text-gray-400 border-2 border-gray-500 dark:border-gray-400 flex justify-center items-center">
+                        <CiCamera size={40} />
+                    </div>
+
+                    {           
+                        currentUser?.user?.id === userId ? 
+                        "No posts yet — start sharing your thoughts!" : 
+                        "Looks like they haven’t shared any posts."
+                    }
                 </div>
-            );
+            )}
+
+            {posts.map((post, index) => {
+                const isLast = index === posts.length - 1;
+                return (
+                    <div
+                    key={post.id}
+                    ref={isLast ? lastPostRef : null}
+                    className="py-2 border-b-[1px] border-light-border dark:border-dark-border"
+                    >
+                    <Post post={post} profile={profile} loading={loadingProfile} />
+                    </div>
+                );
             })}
     
             {loading && (
