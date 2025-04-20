969e891c4f1a02f3f97ae2d788c4443ec7fd11e3
= <phuongnguyen20904@gmail.com>
2025-04-20 15:07:09 +0700

    feat(homepage): implement custom hook to fetch latest posts from followed users

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
-import PostCard from "../components/PostCard";
+import React from "react";
+import { getCurrentUser } from "../helpers/getCurrentUser";
+import useHomepagePost from "../hook/useHomePageFetch";
+import Post from "../components/Post";
+import PostLoading from "../components/Skeleton/PostLoading";
 
 function HomePage() {
+    const { currentUser } = getCurrentUser();
+    const { post, loading, error } = useHomepagePost(currentUser?.user?.id || null);
+
     return (
-    <div className="flex w-full border">    
-        <div className="md:ml-16 lg:ml-0 lg:flex-3 flex-1">
-            <div className=" h-28 border"></div>
-            <PostCard/>
-            <PostCard/> 
-            <PostCard/>
+        <div className="flex w-full">    
+            <div className="md:ml-16 lg:ml-0 lg:flex-3 flex-1 flex flex-col items-center">
+                <div className="w-full h-28"></div>
+                <div className="flex flex-col w-[30rem] md:w-[32rem] lg:w-[45rem]">
+                    {loading ? (
+                        <div className="py-2">
+                            <PostLoading/>
+                            <PostLoading/>
+                        </div>
+                    ) : (
+                        post.map((item, index) => (
+                            <div
+                            key={index}
+                            className="py-2 border-b-[1px] border-light-border dark:border-dark-border"
+                        >
+                        <Post key={item.post.id} post={item.post} profile={item.profile}/>
+                        </div>
+                        ))
+                    )}
+                </div>
+            </div>
+            {/* Sidebar phải */}
+            <div className="lg:flex-1 hidden lg:block border"></div>
         </div>
-        
-        {/* Sidebar phải */}
-        <div className="lg:flex-1 hidden lg:block border"></div>
-    </div>
     );
 }
 
--- frontend/src/services/fetchPost.jsx
+++ frontend/src/services/fetchPost.jsx
@@ -10,7 +10,9 @@ export const fetchPost = async (page = 1, userId = null) => {
         queryParams.append('userId', userId);
         }
 
-        const res = await axios.get(`http://localhost:5000/posts?${queryParams.toString()}`);
+        const res = await axios.get(`http://localhost:5000/posts?${queryParams.toString()}`,{
+            withCredentials: true,
+        });
         return res.data; 
     } catch (err) {
         console.error("Error fetching posts:", err);
@@ -18,3 +20,15 @@ export const fetchPost = async (page = 1, userId = null) => {
     }
 };
 export default fetchPost
+
+export const fetchLatestPost = async (userId) => {
+    try {
+        const res = await axios.get(`http://localhost:5000/posts/${userId}/latestPost`, {
+            withCredentials: true,
+        });
+        return res.data; 
+    } catch (err) {
+        console.error("Error fetching latest post:", err);
+        throw err;
+    }
+};  
