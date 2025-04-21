12c64a12cb80ea2417924723fbbd1e91269ec583
= <phuongnguyen20904@gmail.com>
2025-04-21 11:36:20 +0700

    feat(ConfirmModal, useCreatePostService): add ConfirmModal component and refactor post service import

---
+++ frontend/src/components/Modal/ConfirmModal.jsx
@@ -0,0 +1,43 @@
+// components/Modal/ConfirmDeleteModal.jsx
+import React from 'react';
+
+import { Link } from "react-router-dom";
+
+function ConfirmModal({ title, content, confirm, to, onConfirm, onCancel }) {
+    return (
+        <div className="fixed inset-0 z-[100] bg-black/50 flex items-center justify-center">
+            <div className="bg-white dark:bg-dark-card rounded-xl w-[22rem] text-center dark:text-dark-text overflow-hidden">
+                <div className='w-full py-6 px-4'>
+                    <h2 className="text-xl mb-4">{title}</h2>
+                    <p>{content}</p>
+                </div>
+                <div className="flex flex-col justify-around w-full border-t border-light-border dark:border-gray-500/50">
+                    {to ? (
+                        <Link
+                            to={to}
+                            onClick={onConfirm}
+                            className="h-12 flex items-center justify-center text-red-500 font-semibold dark:hover:bg-dark-hover"
+                        >
+                            {confirm}
+                        </Link>
+                    ) : (
+                        <button
+                            onClick={onConfirm}
+                            className="h-12 px-6 py-3 text-red-500 font-semibold dark:hover:bg-dark-hover"
+                        >
+                            {confirm}
+                        </button>
+                    )}
+                    <button
+                        onClick={onCancel}
+                        className="cursor-pointer border-t border-light-border dark:border-gray-500/50 h-12 px-4 py-2 dark:hover:bg-dark-hover"
+                    >
+                        Cancel
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ConfirmModal;
